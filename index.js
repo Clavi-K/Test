fa989f3dfb33c84359a2b1acfe9e2988a3396a82
Luciano Schmidt <87764819+Clavi-K@users.noreply.github.com>
2022-09-16 11:41:19 -0300

    seguimos probando el flujo (#4)

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ console.log("este vendría a ser el tercer commit")
 console.log("ahora estamos en la rama newBranch")
 
 console.log("testeando el funcionamiento de las reglas")
+
+console.log("seguimos probando el flujo de las ramas")
