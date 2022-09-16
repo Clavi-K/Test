d6c9c3614ace0483834cd3048bbb1a7bd4f19c48
Luciano Schmidt <luciano43schmidt@gmail.com>
2022-09-16 11:36:07 -0300

    ruleTesting

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log("hola buenas tardes")
 console.log("este vendría a ser el tercer commit")
 
 console.log("ahora estamos en la rama newBranch")
+
+console.log("testeando el funcionamiento de las reglas")
